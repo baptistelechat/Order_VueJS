e42695d34957c4a93847feffb19cee78c6818dd3
Baptiste LECHAT <baptiste.lechat@ynov.com>
2020-11-02 20:38:07 +0100

    ✨ Implement Order

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Login :username="Baptiste"/>
+    <Login username="Baptiste"/>
     <Product/>
   </div>
 </template>
@@ -26,6 +25,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 8px;
 }
 </style>
--- src/components/Product.component.vue
+++ src/components/Product.component.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="container">
-        <h1>Produits disponible :</h1>
-        <div class="row">
-            <div class="col-3" v-for="produit in produits" v-bind:key="produit">
-                <p class="jumbotron">
-                    {{produit}}
-                    <br>
-                    <button v-on:click="commander">Commander</button>
-                </p>
+    <div class="row justify-content-center">
+        <div class="col-5-lg col-12-sm">
+            <h1>Produits disponible :</h1>
+            <div class="row">
+                <div class="col-3" v-for="produit in produits" v-bind:key="produit">
+                    <h5 class="jumbotron">
+                        {{produit}}
+                        <br>
+                        <br>
+                        <button class="btn btn-secondary" v-on:click="commander(produit)">Commander</button>
+                    </h5>
+                </div>
             </div>
+        </div>
+        <div class="col-4-lg col-12-sm">
+            <h1 v-if="commandes.length>0">Produits commandés :</h1>
+            <ul class="list-group">
+                <li v-for="commande in commandes" v-bind:key="commande" class="list-group-item">{{commande}}</li>
+            </ul>
         </div>
     </div>
     
@@ -20,19 +29,23 @@
         data() {
             return {
                 produits: ["Pizza","Hamburger","Hot Dog","Tacos"],
-                commande:[]
+                commandes:[]
             }
         },
         methods: {
-            commander: function () { 
-                console.log("ça marche !!!")
-             }
+            commander: function (param) { 
+                this.commandes.push(param);
+                console.log('📝 Ajout de : '+param);
+                console.log('🛒 Commande : '+this.commandes);
+            }
         }
     }
 </script>
 
 <style>
-.produit {
-    color: brown;
+.list-group-item {
+    margin-left: auto;
+    margin-right: auto;
+    width: 250px;
 }
 </style>
